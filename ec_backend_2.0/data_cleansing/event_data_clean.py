978a395967d26ab164097591bfad1f16e10408dc
atodd16 <aaronmtodd95@outlook.com>
2025-01-04 11:22:02 -0500

    initial commit

--- ec_backend_2.0/data_cleansing/event_data_clean.py
+++ ec_backend_2.0/data_cleansing/event_data_clean.py
@@ -7,12 +7,18 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 
 # read in performance_data CSV
-event_data = pd.read_csv(r'C:\Users\aaron\OneDrive\Documents\Golf Modeling\eccentric_goose_model_app\ec_backend_2.0\data_files\raw_data_files\event_data.csv')
+event_data = pd.read_csv(r'C:\Users\aaron\OneDrive\Documents\Golf Modeling\eccentric_goose_model_app\ec_backend_2.0\data_files\raw_data_files\event_data.csv', index_col=0)
 
-# creates new column 'distinct_event_id' which differentiates from tour to tour
+# creates new column 'distinct_event_id' which differentiates event_id from tour to tour
 event_data['distinct_event_id'] = pd.factorize(event_data[['event_id', 'tour']].apply(tuple, axis=1)) [0] + 1
+
+# rename 'date' to 'event_completed'
+event_data.rename(columns={'date':'event_completed'}, inplace=True)
 
 # saves CSV to 'cleansed_data_files'
 event_data.to_csv(r'C:\Users\aaron\OneDrive\Documents\Golf Modeling\eccentric_goose_model_app\ec_backend_2.0\data_files\cleansed_data_files\event_data_cleansed')
 
+#event_data = event_data[event_data['event_name'] == 'Alfred Dunhill Championship #2']
+
+print(event_data)
 
--- ec_backend_2.0/data_cleansing/performance_data_clean.py
+++ ec_backend_2.0/data_cleansing/performance_data_clean.py
@@ -7,9 +7,24 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 
 # read in performance_data CSV
-performance_data = pd.read_csv(r'C:\Users\aaron\OneDrive\Documents\Golf Modeling\eccentric_goose_model_app\ec_backend_2.0\data_files\raw_data_files\performance_data.csv')
+performance_data = pd.read_csv(r'C:\Users\aaron\OneDrive\Documents\Golf Modeling\eccentric_goose_model_app\ec_backend_2.0\data_files\raw_data_files\performance_data.csv', index_col=0,
+                               parse_dates=['event_completed'])
+
+# read in event_data_cleansed CSV
+event_data = pd.read_csv(r'C:\Users\aaron\OneDrive\Documents\Golf Modeling\eccentric_goose_model_app\ec_backend_2.0\data_files\cleansed_data_files\event_data_cleansed', index_col=0,
+                         parse_dates=['event_completed'])
 
 # drops columns where event_id is blank
 performance_data = performance_data.dropna(subset=['event_completed'])
 
-print(performance_data.head(20))
+# merge distinct_event_id into performance_data
+performance_data = performance_data.merge(event_data[['event_id', 'event_completed', 'distinct_event_id']], on=['event_id', 'event_completed'], how='left')
+
+# adds a 'year' column based off 'event_completed'
+performance_data['year'] = pd.to_datetime(performance_data['event_completed']).dt.year.astype(int)
+
+
+print(performance_data)
+
+
+
